a2c832ddd2e0b258c1ccd7ffd2e3286e69f9563d
vandieke <basvdijke@gmail.com>
2024-11-13 10:28:33 +0100

    rocket collision poging

--- TickTick/TickTick/LevelObjects/Enemies/Rocket.cs
+++ TickTick/TickTick/LevelObjects/Enemies/Rocket.cs
@@ -1,5 +1,9 @@
 ﻿using Engine;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 
 /// <summary>
 /// Represents a rocket enemy that flies horizontally through the screen.
@@ -18,6 +22,7 @@ class Rocket : AnimatedGameObject
         LoadAnimation("Sprites/LevelObjects/Rocket/spr_rocket@3", "rocket", true, 0.1f);
         PlayAnimation("rocket");
         SetOriginToCenter();
+        CreateCollider();
 
         sprite.Mirror = facingLeft;
         if (sprite.Mirror)
@@ -31,6 +36,11 @@ class Rocket : AnimatedGameObject
             this.startPosition = startPosition - new Vector2(2 * speed, 0);
         }
         Reset();
+    }
+
+    public void CreateCollider()
+    {
+        //make a collider for the rocket
     }
 
     public override void Reset()
@@ -49,8 +59,81 @@ class Rocket : AnimatedGameObject
         else if (!sprite.Mirror && BoundingBox.Left > level.BoundingBox.Right)
             Reset();
 
+        //code voor aanpassing
+        /*
         // if the rocket touches the player, the player dies
         if (level.Player.CanCollideWithObjects && HasPixelPreciseCollision(level.Player))
             level.Player.Die();
+        */
+
+
+        #region Poging 1, werkte niet
+        /*
+        if (level.Player.CanCollideWithObjects && HasPixelPreciseCollision(level.Player))
+        {
+            Debug.WriteLine("player is on y: " + level.Player.LocalPosition.Y);
+            Debug.WriteLine("were on y: " + LocalPosition.Y);
+            if (level.Player.LocalPosition.Y > LocalPosition.Y)
+            {
+                //we came from above
+                Visible = false;
+                Debug.WriteLine("from above");
+            }
+            else
+            {
+                
+                // we did not.
+                level.Player.Die();
+                Debug.WriteLine("from under"); 
+            }
+        }
+        */
+        #endregion
+
+        #region poging 2, werkt niet
+        /*
+        if (HasPixelPreciseCollision(level.Player))
+        {
+            Rectangle overlap = CollisionDetection.CalculateIntersection(BoundingBox, level.Player.BoundingBox);
+            Debug.WriteLine("Overlap box is : " + overlap.ToString());
+
+            //if we collide horizontally, the overlapbox's width will be smaller than it's height and so the player should die.
+            if (overlap.Width < overlap.Height)
+            {
+                level.Player.Die();
+            }
+            //if this is not the case, the player came from above and so the rocket should die.
+            else
+            {
+                //if the player jumps on top of upper collider and can collide with objects, the rocket dies.
+                Visible = false;
+            }
+        }
+        */
+        #endregion
+
+        #region poging 3, werkt ook niet
+        /*
+        if (level.Player.CanCollideWithObjects && HasPixelPreciseCollision(level.Player))
+        {
+            Debug.WriteLine("Player Bottom Y: " + (level.Player.BoundingBox.Bottom));
+            Debug.WriteLine("Rocket Top Y: " + (BoundingBox.Top));
+
+            //check who's above who
+            bool playerIsAboveRocket = level.Player.BoundingBox.Bottom >= BoundingBox.Top;
+
+            if (playerIsAboveRocket)
+            {
+                //were above the rocket, so reset it and maybe give the player a doublejump?
+                this.Reset();
+                //level.Player.Jump(600);
+            }
+            else
+            {
+                level.Player.Die();
+            }
+        }
+        */
+        #endregion
     }
 }
